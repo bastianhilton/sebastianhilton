0ebcb2032975ef86170ff4c25c10d88b6c03cc7b
Sebastian hilton <bastianh86@outlook.com>
2023-03-05 15:42:57 -0800

    changed layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,37 +43,8 @@ export default defineNuxtConfig({
 
   modules: [
     '@nuxtjs/apollo',
-    //'@sidebase/nuxt-auth',
     '@nuxt/content',
-    'nuxt-meilisearch',
   ],
-
-/*
-  auth: {
-    // The module is enabled. Change this to disable the module
-    isEnabled: true,
-    // The origin is set to the development origin. Change this when deploying to production
-    origin: 'http://localhost:3001',
-    // The base path to the authentication endpoints. Change this if you want to add your auth-endpoints at a non-default location
-    basePath: '/api/auth',
-    // Whether to periodically refresh the session. Change this to `true` for a refresh every seconds or set this to a number like `5000` for a refresh every 5000 milliseconds (aka: 5 seconds)
-    enableSessionRefreshPeriodically: true,
-    // Whether to refresh the session whenever a window focus event happens, i.e, when your user refocuses the window. Set this to `false` to turn this off
-    enableSessionRefreshOnWindowFocus: true,
-    // Whether to add a global authentication middleware that will protect all pages without exclusion
-    enableGlobalAppMiddleware: false
-  }, 
-*/
-
-  meilisearch: {
-    hostUrl:  process.env.HOST_URL,
-    searchApiKey: process.env.SEARCH_API,
-    adminApiKey: process.env.ADMIN_API,
-    serverSideUsage: true,
-    instantSearch: {
-      theme: 'algolia'
-    }
-  },
 
   apollo: {
     clients: {
