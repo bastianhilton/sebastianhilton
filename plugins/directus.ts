9c8a6ac1e0f3e1da57d95128358d12af61d9dfec
Sebastian hilton <bastianh86@outlook.com>
2025-06-17 02:23:06 +0100

    updated website from the beginning, add new layout, pages, updated Nuxt 3

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -1,80 +1,12 @@
-import {
-	createDirectus,
-	rest,
-	readItem,
-	readItems,
-	createItem,
-	deleteItem,
-	uploadFiles,
-	readSingleton,
-	readFieldsByCollection,
-	registerUser,
-	authentication,
-	readMe,
-	refresh,
-	withToken,
-	type AuthenticationStorage
-} from '@directus/sdk';
+import { createDirectus, rest, readItem, readItems, createItem, deleteItem, uploadFiles, readSingleton } from '@directus/sdk';
 import "dotenv"
-import type {
-	DirectusSchema
-} from '~/types/directus';
 
 export default defineNuxtPlugin(() => {
-	const config = useRuntimeConfig()
-	class NuxtCookieStorage {
-		cookie = useCookie("directus-data");
-		get() {
-			return this.cookie.value;
-		}
-		set(data: any) {
-			this.cookie.value = data;
-		}
-	}
+const config = useRuntimeConfig()
 
-	const storage = new NuxtCookieStorage() as AuthenticationStorage;
-
-	const directus = createDirectus < DirectusSchema > (`${config.public.directus.url}`)
-		.with(authentication("cookie", {
-			credentials: "include",
-			storage
-		}))
-		.with(rest({
-			credentials: "include"
-		}));
-
-		const isAuthenticated = async () => {
-		try {
-			const me = await directus.request(readMe());
-			return me;
-		} catch (error) {
-			console.error(error)
-			return false;
-		}
-	};
-
-	const refreshToken = async () => {
-		return directus.request(
-			refresh('cookie')
-		);
-	};
-
-	const logout = async () => {
-		await directus.logout()
-		navigateTo('/')
-	}
-
+const directus = createDirectus(`${config.public.directus.url}`).with(rest());
+	
 	return {
-		provide: {
-			directus,
-			readItem,
-			readItems,
-			createItem,
-			deleteItem,
-			uploadFiles,
-			readSingleton,
-			readFieldsByCollection,
-			registerUser, isAuthenticated, refreshToken, logout, withToken
-		},
+		provide: { directus, readItem, readItems, createItem, deleteItem, uploadFiles, readSingleton },
 	};
 });
